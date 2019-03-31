2ee0da4997b3cc313e985a2f7a8d9c0f9cb03feb
Ary-El Durán <aryelduranballestero@gmail.com>
2019-03-31 16:29:07 -0600

    Modficaciones en la ventana

--- ProyectoEED/ProyectoEED/main.cpp
+++ ProyectoEED/ProyectoEED/main.cpp
@@ -1,11 +1,20 @@
 #include "Graph.h"
 #include "Simple_window.h"
 
+typedef struct Nodo* enlace;
+struct Nodo {
+	int v;
+	enlace sig;
+	Nodo(int v) : v{ v }, sig{ nullptr } {}
+	Nodo(int v, enlace sig) : v{ v }, sig{ sig }{}
+};
+
 int main()
 {
 	using namespace Graph_lib;
 
 	Point tl(150, 150);
-	Simple_window win(tl, 600, 400, "My window");
+	Simple_window win(tl, 750, 450, "My window");
+
 	win.wait_for_button();
 }
